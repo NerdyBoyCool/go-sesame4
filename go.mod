846d2434e30df5edcfd327f78515911ded3063f3
Kurt <kurt@timetreeapp.com>
2022-06-18 18:15:09 +0900

    feat: 🎸 lock sesame

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NerdyBoyCool/go-sesame4
 
 go 1.17
+
+require github.com/chmike/cmac-go v1.1.0
--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/chmike/cmac-go"
 )
 
 const notifyMessage string = "\n施錠に失敗しました。\n"
@@ -73,7 +83,7 @@ func Device() (*Sesame, error) {
 		return nil, err
 	}
 
-	apiKey := os.Getenv("SESAME_DEVELOPE_API_KEY")
+	apiKey := os.Getenv("SESAME_DEVELOPER_API_KEY")
 	if apiKey == "" {
 		panic("SESAME_DEVELOPE_API_KEY is not set.")
 	}
@@ -95,6 +105,34 @@ func Device() (*Sesame, error) {
 	return &s, err
 }
 
+type Param struct {
+	Cmd     int16  `json:"cmd"`
+	History string `json:"history"`
+	Sign    string `json:"sign"`
+}
+
+func toLittleEndian(t int64) []byte {
+	i := int32(t)
+	b := make([]byte, 4)
+	binary.LittleEndian.PutUint32(b, uint32(i))
+	return b
+}
+
+func toCMAC(secretKey string) (string, error) {
+	message := toLittleEndian(time.Now().Unix())
+	byteKey, err := hex.DecodeString(secretKey)
+	if err != nil {
+		return "", err
+	}
+	cm, err := cmac.New(aes.NewCipher, byteKey)
+	if err != nil {
+		return "", err
+	}
+	cm.Write(message[1:4])
+	m := cm.Sum(nil)
+	return hex.EncodeToString(m), nil
+}
+
 func main() {
 	var s *Sesame
 	s, err := Device()
@@ -105,5 +143,33 @@ func main() {
 	if s.BatteryPercentage < 20 {
 		notifyLine("バッテリー残量が少なくなっています。")
 	}
+
+	key := os.Getenv("SESAME_SECRET_KEY")
+	m, err := toCMAC(key)
+	if err != nil {
+		fmt.Errorf("cannot get cmac: %w", err)
+		return
+	}
+	apiKey := os.Getenv("SESAME_DEVELOPER_API_KEY")
+	from := base64.StdEncoding.EncodeToString([]byte("by API"))
+	param := &Param{Cmd: 88, Sign: m, History: from}
+	data, _ := json.Marshal(param)
+	deviseUUID := os.Getenv("SESAME_UUID")
+	req, err := http.NewRequest("POST", "https://app.candyhouse.co/api/sesame2/"+deviseUUID+"/cmd", bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Errorf("cannot create HTTP request: %w", err)
+		return
+	}
+	req.Header.Set("x-api-key", apiKey)
+	var client *http.Client = &http.Client{}
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		fmt.Errorf("Request Error: %w", err)
+		return
+	}
+
+	// body, _ := io.ReadAll(res.Body)
+	// fmt.Println(string(body))
 
 }
